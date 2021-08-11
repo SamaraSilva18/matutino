41152bb95baedf430cdfcefa58127cdd32ffb8bf
SamaraSilva18 <samarasilvacandy@gmail.com>
2021-08-11 14:08:49 +0000

    (11/08) Pagina Dividida

--- menu-horizontal/src/componentes/App.js
+++ menu-horizontal/src/componentes/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Menu from './Menu.js';
+import Secoes from './Secoes.js';
 
 function App() {
   return (
     <div>
-      <Menu />
+      <Secoes />
   
     </div>
   );
---
+++ menu-horizontal/src/componentes/Secoes.js
@@ -0,0 +1,60 @@
+import React from "react"
+import "./Secoes.css"
+
+export default function Secoes() {
+    return (
+        <div>
+            <div className="menu">
+                <nav className="navMenu">
+                    <ul>
+                        <li> <a href = "#inicio"> Nosso Canal </a> </li>
+                        <li> <a href = "#cursos"> Cursos </a> </li>
+                        <li> <a href = "#contatos"> Contatos </a> </li>
+                        <li> <a href = "#parceiro"> Parceiros </a> </li>
+                        <li> <a href = "#missao"> Missao </a> </li> 
+                        <li> <a href = "#sobre"> Sobre </a> </li>
+                    </ul>
+                </nav>
+
+            </div>
+
+            <section id="inicio">
+                <h2> Nosso Canal </h2> 
+            </section>
+            
+            <section id="cursos">
+                <h2> Cursos </h2> 
+            </section>
+
+            <section id="contatos">
+                <h2> Contatos </h2> 
+            </section>
+
+            <section id="parceiros">
+                <h2> Parceiros </h2> 
+            </section>
+
+            <section id="missao">
+                <h2> Missao </h2> 
+            </section>
+
+            <section id="sobre">
+                <h2> Sobre </h2> 
+            </section>
+
+            <a class = "voltarTopo" href = ".menu"> Topo </a>
+            <a class = "irInicio" href = "#inicio"> Inicio </a>
+            <a class = "irCursos" href = "#cursos"> Cursos </a>
+            <a class = "irContatos" href = "#contatos"> Contatos </a>
+            <a class = "irParceiros" href = "#parceiros"> Parceiros </a>
+            <a class = "irMissao" href = "#missao"> Missao </a>
+            <a class = "irSobre" href = "#sobre"> Sobre </a>
+
+            <footer>
+               <h3> Rodape </h3>
+            </footer>
+
+        </div>
+    )
+}
+
